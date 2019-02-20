b7a240efe266b97c151542677359766c8df6831e
Laurie Anderson <plipandfirt@gmail.com>
2019-02-19 20:17:39 -0500

    added Build

---
+++ build/precache-manifest.8f17a71c7db55a8d9821ef9f24a40aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d9bd2b579002b14cab06",
+    "url": "/static/js/main.d9bd2b57.chunk.js"
+  },
+  {
+    "revision": "30ea3e140c39df720bf2",
+    "url": "/static/js/2.30ea3e14.chunk.js"
+  },
+  {
+    "revision": "d9bd2b579002b14cab06",
+    "url": "/static/css/main.9b675282.chunk.css"
+  },
+  {
+    "revision": "4b5109d0344b89c478a6ae16d9bfc4b8",
+    "url": "/index.html"
+  }
+];
